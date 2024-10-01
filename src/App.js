57f80b1b248cb4e77efd5b37efc3c26132cb4cfd
Tri040422 <huynhminhtri20041@gmail.com>
2024-10-01 20:38:32 +0700

    Lab3.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import '@fortawesome/fontawesome-free/css/all.css';
-import Header from './Header';
-import SelectedItemsPopup from './SelectedItemsPopup';
-import CardItem from './CardItem';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "@fortawesome/fontawesome-free/css/all.css";
+import Header from "./Header";
+import SelectedItemsPopup from "./SelectedItemsPopup";
+import CardItem from "./CardItem";
 
 function App() {
   const [cart, setCart] = useState({});
@@ -13,13 +13,15 @@ function App() {
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
-    const storedCart = JSON.parse(localStorage.getItem('cart')) || {};
+    const storedCart = JSON.parse(localStorage.getItem("cart")) || {};
     setCart(storedCart);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart));
-    setTotalItems(Object.values(cart).reduce((sum, item) => sum + item.count, 0));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    setTotalItems(
+      Object.values(cart).reduce((sum, item) => sum + item.count, 0)
+    );
   }, [cart]);
 
   const addToCart = (item) => {
@@ -48,45 +50,99 @@ function App() {
   return (
     <>
       <Header totalItems={totalItems} togglePopup={togglePopup} />
-      <div className='container-fluid bg-dark text-white'>
+      <div className="container-fluid bg-dark text-white">
         <div className="row">
           <div className="col-lg-12 mb-5">
-            <div className='row'>
+            <div className="row">
               <div id="carouselExampleCaptions" className="carousel slide">
                 <div className="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide-to="0"
+                    className="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                  ></button>
                 </div>
                 <div className="carousel-inner">
                   <div className="carousel-item active">
-                    <img src="./Images/pizza1.jpg" className="d-block w-100" alt="..."/>
+                    <img
+                      src="./Images/pizza1.jpg"
+                      className="d-block w-100"
+                      alt="..."
+                    />
                     <div className="carousel-caption d-none d-md-block">
                       <h5>First slide label</h5>
-                      <p>Some representative placeholder content for the first slide.</p>
+                      <p>
+                        Some representative placeholder content for the first
+                        slide.
+                      </p>
                     </div>
                   </div>
                   <div className="carousel-item">
-                    <img src="./Images/pizza2.jpg" className="d-block w-100" alt="..."/>
+                    <img
+                      src="./Images/pizza2.jpg"
+                      className="d-block w-100"
+                      alt="..."
+                    />
                     <div className="carousel-caption d-none d-md-block">
                       <h5>Second slide label</h5>
-                      <p>Some representative placeholder content for the second slide.</p>
+                      <p>
+                        Some representative placeholder content for the second
+                        slide.
+                      </p>
                     </div>
                   </div>
                   <div className="carousel-item">
-                    <img src="./Images/pizza3.jpg" className="d-block w-100" alt="..."/>
+                    <img
+                      src="./Images/pizza3.jpg"
+                      className="d-block w-100"
+                      alt="..."
+                    />
                     <div className="carousel-caption d-none d-md-block">
                       <h5>Third slide label</h5>
-                      <p>Some representative placeholder content for the third slide.</p>
+                      <p>
+                        Some representative placeholder content for the third
+                        slide.
+                      </p>
                     </div>
                   </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
                   <span className="visually-hidden">Previous</span>
                 </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions"
+                  data-bs-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
                   <span className="visually-hidden">Next</span>
                 </button>
               </div>
@@ -98,11 +154,31 @@ function App() {
               <h2>Our Menu</h2>
               <div className="row">
                 {[
-                  { id: 1, title: "Margherita Pizza", price: 24.00, imgSrc: "./Images/menu1.jpg" },
-                  { id: 2, title: "Mushroom Pizza", price: 25.00, imgSrc: "./Images/menu2.jpg" },
-                  { id: 3, title: "Hawaiian Pizza", price: 30.00, imgSrc: "./Images/menu3.jpg" },
-                  { id: 4, title: "Pesto Pizza", price: 30.00, imgSrc: "./Images/menu4.jpg" },
-                ].map(item => (
+                  {
+                    id: 1,
+                    title: "Margherita Pizza",
+                    price: 24.0,
+                    imgSrc: "./Images/menu1.jpg",
+                  },
+                  {
+                    id: 2,
+                    title: "Mushroom Pizza",
+                    price: 25.0,
+                    imgSrc: "./Images/menu2.jpg",
+                  },
+                  {
+                    id: 3,
+                    title: "Hawaiian Pizza",
+                    price: 30.0,
+                    imgSrc: "./Images/menu3.jpg",
+                  },
+                  {
+                    id: 4,
+                    title: "Pesto Pizza",
+                    price: 30.0,
+                    imgSrc: "./Images/menu4.jpg",
+                  },
+                ].map((item) => (
                   <CardItem key={item.id} item={item} addToCart={addToCart} />
                 ))}
               </div>
@@ -111,14 +187,24 @@ function App() {
 
           <div className="col-lg-12">
             <div className="container">
-              <div className='row'>
-                <h2 className='text-center'>Book your Table</h2>
+              <div className="row">
+                <h2 className="text-center">Book your Table</h2>
                 <div className="row mb-3">
                   <div className="col">
-                    <input type="text" className="form-control" placeholder="Your name" aria-label="Your name" />
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Your name"
+                      aria-label="Your name"
+                    />
                   </div>
                   <div className="col">
-                    <input type="text" className="form-control" placeholder="Your email" aria-label="Your email" />
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Your email"
+                      aria-label="Your email"
+                    />
                   </div>
                   <div className="col">
                     <select id="inputState" className="form-select">
@@ -129,22 +215,35 @@ function App() {
                 </div>
                 <div className="row mb-3">
                   <div className="col">
-                    <textarea className="form-control" rows="5" placeholder="Please write your comment" aria-label="Please write your comment"></textarea>
+                    <textarea
+                      className="form-control"
+                      rows="5"
+                      placeholder="Please write your comment"
+                      aria-label="Please write your comment"
+                    ></textarea>
                   </div>
                 </div>
                 <div className="row">
                   <div className="col">
-                    <input type='submit' className='btn btn-warning text-white' value="Send Message"></input>
+                    <input
+                      type="submit"
+                      className="btn btn-warning text-white"
+                      value="Send Message"
+                    ></input>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
 
       {showPopup && (
-        <SelectedItemsPopup cart={cart} onClose={togglePopup} onQuantityChange={handleQuantityChange} />
+        <SelectedItemsPopup
+          cart={cart}
+          onClose={togglePopup}
+          onQuantityChange={handleQuantityChange}
+        />
       )}
     </>
   );
